b3ed74f4fbd3f8beea161b4bf2f8a0a1322a23d5
padraigbowl <80794599+padraigbowl@users.noreply.github.com>
2022-01-04 12:03:46 +0000

    Update FightMilkTycoon.cpp

--- FightMilkTycoon.cpp
+++ FightMilkTycoon.cpp
@@ -6,29 +6,26 @@ using namespace std;
     /*
     Ingredients: Crow Eggs, Milk, Vodka,  Cups, Ice Cubes
 
-     *
-    Costs:  , 0.03 ice cube, 0.23 cup,
-     *
-     *int 16 Crow Eggs     $1.44
-     *int 1 litre milk     $0.80
-     *double 0.5 litre cream  $1.10
-     *double 8 ounces vodka   $5.25 1 Bottle = 16 ounces
-     *int 10 bottles          $2.30
-                        $10.89
+// double EGGCOST = 0.09, MILKCOST = 0.80, CREAMCOST = 2.20, VODKACOST = 20.30, BOTTLECOST = 0.23;     *
+
+     *16 Crow Eggs          $1.44
+     *1 litre milk          $0.80
+     *0.5 litre cream       $1.10
+     *18 ounces vodka       $10.15 1 Bottle = 36 ounces
+     *10 bottles            $2.30
+     *total/per bottle      $15.80/1.58
 
     */
 
 void inventory(int crowEggs,int milk,int vodka,int cream,int emptyBottles,int fullBottles);
 
 
-// double EGGCOST = 0.09, MILKCOST = 0.80, CREAMCOST = 2.20, VODKACOST = 15.75, BOTTLECOST = 0.23;
+
 int main(){
 
 int crowEggs = 32, milk = 2, emptyBottles = 20, fullBottles = 0, vodka = 36, cream = 1000;
 
 inventory(crowEggs, milk, vodka, cream, emptyBottles, fullBottles);
-
-Sleep(2000);
 
 	cout << "Press any key to make Some fight milk\n";
 cin.ignore();
@@ -39,8 +36,6 @@ cin.ignore();
 	fullBottles = fullBottles + 10;
 	vodka = vodka - 18;
 	cream = cream - 500;
-
-Sleep(2000);
 
 inventory(crowEggs, milk, vodka, cream, emptyBottles, fullBottles);
 system("pause");
@@ -60,7 +55,7 @@ void inventory(int crowEggs,int milk,int vodka,int cream,int emptyBottles,int fu
 	cout << "Empty Bottles: " << emptyBottles << "\n";
 	cout << "Full Bottles:  " << fullBottles << "\n";
 }
-/*
+/*This needs to be a class method
 void makeFightMilk(int crowEggs,int milk,int vodka,int cream,int emptyBottles,int fullBottles)
 {
 void makeFightMilk(int crowEggs,int milk,int vodka,int cream,int emptyBottles,int fullBottles);
